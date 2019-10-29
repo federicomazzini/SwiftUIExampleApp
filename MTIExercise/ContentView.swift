64f92665157c026c899fcc85befc97c4a9e56383
Federico Mazzini <federico.mazzini@lateralview.net>
2019-10-29 15:37:38 -0300

    Initial Commit

---
+++ MTIExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MTIExercise
+//
+//  Created by federico mazzini on 29/10/19.
+//  Copyright © 2019 federico mazzini. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
